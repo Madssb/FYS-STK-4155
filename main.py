b3ce8227a350d7e8a5862027578f48c42cb62bb6
mads <anderen72@gmail.com>
2023-09-27 19:18:09 +0200

    working code for OLS, Ridge, and Lasso regression

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ Solve project here
 """
 import numpy as np
 from utilities import (franke_function, mean_squared_error, r2_score, my_figsize)
-from regression import (ols_regression, features_polynomial_xy, ridge_regression)
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
@@ -13,99 +12,6 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 from regression import LinearRegression2D
-
-
-def ols_franke_function():
-  """
-  Create a mesh for the franke function, and convert aforementioned mesh to
-  some mock data by adding gaussian noise. Next, split data into trainining set
-  and test set, and train for optimal parameters with OLS using training set,
-  for complexities spanning polynomial order one to five. 
-  Then evaluate models with MSE and R2, and visualize aforementioned quantities
-  as function of the complexity.
-
-
-  Returns
-  -------
-  None
-
-
-  """
-  # Make data.
-  x = np.arange(0, 1, 0.05)
-  y = np.arange(0, 1, 0.05)
-  x_mesh, y_mesh = np.meshgrid(x, y)
-  analytic = franke_function(x_mesh, y_mesh)
-  noise = np.random.normal(0, 1, x_mesh.shape)
-  polynomial_degrees = np.linspace(1, 5, 5, dtype=int)
-  mses = np.empty_like(polynomial_degrees, dtype=float)
-  r2s = np.empty_like(polynomial_degrees, dtype=float)
-  mock_data = analytic + noise
-  data_flat = mock_data.ravel()
-  for idx, polynomial_degree in enumerate(polynomial_degrees):
-    features = features_polynomial_xy(
-        x, y, polynomial_degree, scale=False)
-    features_train, features_test, data_train, data_test = (
-      train_test_split(features, data_flat))
-    ols_parameters = ols_regression(features_train, data_train)
-    ols_model_predicted = features_test @ ols_parameters
-    mses[idx] = mean_squared_error(data_test, ols_model_predicted)
-    r2s[idx] = r2_score(data_test, ols_model_predicted)
-    print(
-        f"degree {polynomial_degree}: mse = {mses[idx]:.4g}, r2 = {r2s[idx]:.4g}")
-  plt.plot(polynomial_degrees, mses, label="MSE")
-  plt.plot(polynomial_degrees, r2s, label="R2")
-
-  plt.legend()
-  plt.show()
-
-
-def ridge_franke_function():
-  """
-  Create a mesh for the franke function, and convert aforementioned mesh to
-  some mock data by adding gaussian noise. Next, split data into trainining set
-  and test set, and train for optimal parameters with using regression, for
-  complexities spanning one to five, and for various hyperparameters. Then
-  evaluate models with MSE and r2.
-  """
-  x = np.arange(0, 1, 0.05)
-  y = np.arange(0, 1, 0.05)
-  x_mesh, y_mesh = np.meshgrid(x, y)
-  analytic = franke_function(x_mesh, y_mesh)
-  noise = np.random.normal(0, 1, x_mesh.shape)
-  mock_data = analytic + noise
-  data_flat = mock_data.ravel()
-  polynomial_degrees = np.linspace(1, 5, 5, dtype=int)
-  #hyperparams = np.linspace(10**-4,10**4,10)
-  hyperparams = np.logspace(-4,4,10)
-  mse = np.empty((polynomial_degrees.shape[0],hyperparams.shape[0]),dtype=float)
-  r2s = np.empty_like(mse, dtype=float)
-  for pol_idx, polynomial_degree in enumerate(polynomial_degrees):
-    features = features_polynomial_xy(
-        x, y, polynomial_degree, scale=False)
-    features_train, features_test, data_train, data_test = (
-      train_test_split(features, data_flat))
-    for hyp_idx, hyperparam in enumerate(hyperparams):
-      ridge_parameters = ridge_regression(
-        features_train,data_train, hyperparam)
-      rigdge_model_predicted = features_test @ ridge_parameters
-      mse[pol_idx,hyp_idx] = mean_squared_error(data_test, rigdge_model_predicted)
-      r2s[pol_idx, hyp_idx] = r2_score(data_test, rigdge_model_predicted)
-  
-  fig, ax = plt.subplots(figsize=my_figsize())
-  polynomial_degrees_mesh, hyperparams_mesh = np.meshgrid(polynomial_degrees, hyperparams)
-  levels = np.linspace(mse.min(),mse.max(), 7)
-  contour = ax.contourf(polynomial_degrees_mesh, hyperparams_mesh, mse.T, levels=levels)
-  ax.set_yscale("log")
-  ax.set_xlabel("complexity")
-  ax.set_ylabel("Ridge parameter")
-  ax.grid()
-  format_func = lambda x, _: f"{x:.2f}"
-  cbar = plt.colorbar(contour, format=format_func)
-  fig.tight_layout()
-  fig.savefig("ridge_mse.pdf")
-
-
 
 
 def main():
@@ -119,8 +25,12 @@ def main():
   hyperparameters = np.logspace(-4,4,10)
   linreg_instance = LinearRegression2D(x, y, mock_data,
                                        degrees, hyperparameters)
+  # Task a
   linreg_instance.visualize_mse_ols()
+  # Task b
   linreg_instance.visualize_mse_ridge()
+  # Task c
+  linreg_instance.visualize_mse_lasso()
 
 
 if __name__ == '__main__':
--- regression.py
+++ regression.py
@@ -3,9 +3,11 @@ This model contains the regression functions pertaining to project 1.
 """
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Lasso
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
 from utilities import my_figsize
+from typing import Tuple
 
 
 def mean_squared_error(y: np.ndarray, model: np.ndarray) -> float:
@@ -54,140 +56,10 @@ def r2_score(y: np.ndarray, model: np.ndarray) -> float:
   return 1 - mse**2/mean_squared_error(y, mean_y)
 
 
-def features_polynomial_xy(x: np.ndarray, y: np.ndarray, degree: int,
-                           scale=True) -> np.ndarray:
-  """
-  Construct design matrix for 2 dim polynomial:
-  (1 + y + ... y**p) + x(1 + y + ... y**p) + ... + x**p(1 + y + ... y**p),
-  where p is degree for polynomial.
-
-
-  Parameters
-  ----------
-  x: one-dimensional array of floats
-    X-dimension mesh
-  y: one-dimensional array of floats
-    Y-dimension mesh.
-  polynomial_degree
-    Polynomial degree for model.
-  scale
-    True if scaling data, false if not.
-
-
-  Returns
-  two-dimensional-array
-    Design matrix for two dimensional polynomial of specified degree. 
-
-  
-  """
-  assert len(x.shape) == 1, "requires n dimensional array."
-  assert len(y.shape) == 1, "requires n dimensional array."
-  assert isinstance(degree, (int, np.int64))
-  len_x = x.shape[0]
-  len_y = y.shape[0]
-  features_xy = np.empty((len_x*len_y, (degree+1)**2), dtype=float)
-  for i, x_ in enumerate(x):
-    for j, y_ in enumerate(y):
-      row = len_y*i + j
-      for k in range(degree + 1):
-        for l in range(degree + 1):
-          col = k*(degree+1) + l
-          features_xy[row, col] = x_**k*y_**l
-  if scale:
-    features_xy -= np.mean(features_xy, axis=1, keepdims=True)
-  return features_xy
-
-
-def test_features_polynomial_xy():
-  """ 
-  Ensures features_polynomial_xy() is working as intended.
-  """
-  x = np.array([2, 3])
-  y = np.array([4, 5])
-  expected_features = np.array([[1, 4, 2, 8],
-                                [1, 5, 2, 10],
-                                [1, 4, 3, 12],
-                                [1, 5, 3, 15]])
-  features = features_polynomial_xy(x, y, 1, scale=False)
-  assert features.shape == (4, 4)
-  print(features)
-  assert (features == expected_features).all()
-
-
-def ols_regression(features: np.ndarray, y: np.ndarray) -> np.ndarray:
-  """
-  Compute the optimal parameters per Ordinary Least Squares regression.
-
-
-  Parameters
-  ----------
-  features: two-dimensional array of floats
-    design matrix for n-dimensional mesh
-    Two dimensional numpy array
-  y: one-dimensional array of floats
-    n-dimensional mesh function linearized
-  
-  
-  Returns
-  -------
-  numpy.ndarray
-      Optimal parameters as predicted by Ridge.
-
-
-  Raises
-  ------
-  AssertionError
-      shapes of features or y are not permitted.
-
-    
-  """
-  assert len(features.shape) == 2, "requires nxm dimensional array."
-  assert len(y.shape) == 1, "requires n dimensional array."
-  return np.linalg.pinv(
-      np.transpose(features) @ features
-  ) @ np.transpose(features) @ y
-
-
-def ridge_regression(features: np.ndarray, y: np.ndarray,
-                     hyperparameter: float) -> np.ndarray:
-  """
-  Computes the optimal parameters per Ridge regression.
-
-  Parameters
-  ----------
-  features
-    Two-dimensional numpy array
-  y
-    One-dimensional numpy array
-  hyperparameter
-    TBA
-
-  Returns
-  -------
-  numpy.ndarray
-    Optimal parameters
-
-  Raises
-  ------
-  AssertionError
-    shapes of features or y are not permitted, or hyperparameter is not
-    float.
-
-
-  """
-  assert len(features.shape) == 2, "requires nxm dimensional array."
-  assert len(y.shape) == 1, "requires n dimensional array"
-  assert isinstance(hyperparameter, float), "must be float"
-  return np.linalg.pinv(
-      np.transpose(features) @ features
-      + np.identity(features.shape[1])*hyperparameter
-  ) @ np.transpose(features) @ y
-
-
 class LinearRegression2D:
   def __init__(self, x: np.ndarray, y: np.ndarray, z: np.ndarray,
                degrees: np.ndarray, hyperparameters: np.ndarray,
-               scale=True):
+               center=True):
     """
     Parameters
     ----------
@@ -197,6 +69,12 @@ class LinearRegression2D:
       Y-dimension mesh.
     z: two-dimensional array of floats
       Mesh function z(x,y)
+    degrees: one-dimensional array of integers
+      complexities span for linear regression models.
+    hyperparameters: one-dimensional array of floats
+      hyperparameter span for linear regression models.
+    center: Bool
+      True if centering features.
     """
     assert len(x.shape) == 1, "requires m dimensional array."
     assert len(y.shape) == 1, "requires n dimensional array."
@@ -210,14 +88,15 @@ class LinearRegression2D:
     self.y = y
     self.z = z
     self.z_flat = z.ravel()
-    self.scale = scale
+    self.center = center
 
   def features_polynomial_xy(
           self, x: np.ndarray, y: np.ndarray, degree: int) -> np.ndarray:
     """
-    Construct design matrix for 2 dim polynomial:
+    Construct design matrix for two-dimensional polynomial, where columns are:
     (1 + y + ... y**p) + x(1 + y + ... y**p) + ... + x**p(1 + y + ... y**p),
-    where p is degree for polynomial.
+    where p is degree for polynomial, x = x_i and y = y_j, indexed such that
+    row index k  =  len(y)*i + j.
 
 
     Parameters
@@ -226,13 +105,12 @@ class LinearRegression2D:
       X-dimension mesh
     y: one-dimensional array of floats
       Y-dimension mesh.
-    polynomial_degree
+    degree: int
       Polynomial degree for model.
-    scale
-      True if scaling data, false if not.
 
 
     Returns
+    -------
     two-dimensional-array
       Design matrix for two dimensional polynomial of specified degree. 
 
@@ -251,13 +129,15 @@ class LinearRegression2D:
           for l in range(degree + 1):
             col = k*(degree+1) + l
             features_xy[row, col] = x_**k*y_**l
-    if self.scale:
+    if self.center:
       features_xy -= np.mean(features_xy, axis=1, keepdims=True)
     return features_xy
 
-  def ols_regression(self, degree: int) -> np.ndarray:
+  def ols(self, degree: int) -> Tuple[np.ndarray, float, float]:
     """
-    Compute the optimal parameters per Ordinary Least Squares regression.
+    Partition data into training set and test set, compute optimal parameters 
+    with ordinary least squares regression using training set, and compute
+    mean squared error and R-squared score for trained model with test set. 
 
 
     Parameters
@@ -289,22 +169,33 @@ class LinearRegression2D:
     r2 = r2_score(z_test, predicted_model)
     return predicted_model, mse, r2
 
-  def ridge_regression(self, degree: int, hyperparameter: float) -> np.ndarray:
+  def ridge(self, degree: int, hyperparameter: float
+            ) -> Tuple[np.ndarray, float, float]:
     """
-    Computes the optimal parameters per Ridge regression.
+    Partition data into training set and test set, compute optimal parameters 
+    with ridge regression using training set, and compute mean squared error
+    and R-squared score for trained model with test set. 
 
+    
     Parameters
     ----------
-    degree: float
+    degree: int
       degree of complexity for polynomial.
     
-    hyperparameter
-      TBA
+    hyperparameter: float
+      hyperparameter
+      
 
     Returns
     -------
     numpy.ndarray
-      Optimal parameters
+    Tuple[np.ndarray, float, float]
+        predicted_model: numpy.ndarray
+            Predicted values for the test set.
+        mse: float
+            Mean Squared Error.
+        r2: float
+            R-squared score.
 
 
     """
@@ -320,6 +211,50 @@ class LinearRegression2D:
     r2 = r2_score(z_test, predicted_model)
     return predicted_model, mse, r2
 
+  def lasso(self, degree: int, hyperparameter: float
+            ) -> Tuple[np.ndarray, float, float]:
+    """
+    Partition data into training set and test set, compute optimal parameters 
+    with ridge regression using training set, and compute mean squared error
+    and R-squared score for trained model with test set. 
+
+    
+    Parameters
+    ----------
+    degree: int
+      degree of complexity for polynomial.
+    
+    hyperparameter: float
+      hyperparameter
+      
+
+    Returns
+    -------
+    numpy.ndarray
+    Tuple[np.ndarray, float, float]
+        predicted_model: numpy.ndarray
+            Predicted values for the test set.
+        mse: float
+            Mean Squared Error.
+        r2: float
+            R-squared score.
+
+
+    """
+    features = self.features_polynomial_xy(self.x, self.y, degree)
+    features_train, features_test, z_train, z_test = train_test_split(
+        features, self.z_flat)
+    model = Lasso()
+    model.fit(features_train, z_train)
+    predicted_model = model.predict(features_test)
+    # optimal_parameters = np.where(
+    #   z_train > hyperparameter/2, z_train - hyperparameter/2, 
+    #   np.where(z_train < -hyperparameter/2, z_train + hyperparameter/2, 0))
+    # predicted_model = features_test @ optimal_parameters
+    mse = mean_squared_error(z_test, predicted_model)
+    r2 = r2_score(z_test, predicted_model)
+    return predicted_model, mse, r2 
+
   def mse_and_r2_ols(self):
     """
     Compute mean squared error for ordinary least square regression computed
@@ -328,19 +263,9 @@ class LinearRegression2D:
     self.mses_ols = np.empty_like(self.degrees, dtype=float)
     self.r2s_ols = np.empty_like(self.degrees, dtype=float)
     for i, degree in enumerate(self.degrees):
-      model, mse, r2 = self.ols_regression(degree)
+      model, mse, r2 = self.ols(degree)
       self.mses_ols[i] = mse
       self.r2s_ols[i] = r2
-
-  def visualize_mse_ols(self):
-    if not hasattr(self, 'mses_ols'):
-      self.mse_and_r2_ols()
-    fig, ax = plt.subplots(figsize=my_figsize())
-    ax.plot(self.degrees, self.mses_ols, label="MSE")
-    ax.set_xlabel("Complexity")
-    ax.set_ylabel("MSE")
-    fig.tight_layout()
-    fig.savefig("mse_ols.pdf")
 
   def mse_and_r2_ridge(self):
     """
@@ -352,24 +277,70 @@ class LinearRegression2D:
     self.r2s_ridge = np.empty_like(self.mses_ridge, dtype=float)
     for i, degree in enumerate(self.degrees):
       for j, hyperparameter in enumerate(self.hyperparameters):
-        model, mse, r2 = self.ridge_regression(degree, hyperparameter)
+        model, mse, r2 = self.ridge(degree, hyperparameter)
         self.mses_ridge[i, j] = mse
         self.r2s_ridge[i, j] = r2
+    return self.mses_ridge, self.r2s_ridge
+  
+  def mse_and_r2_lasso(self):
+    """
+    Compute mean squared error for Lasso regression computed models as a
+    function of model complexity.
+    """
+    self.mses_lasso = np.empty(
+      (self.degrees.shape[0], self.hyperparameters.shape[0]), dtype=float)
+    self.r2s_lasso = np.empty_like(self.mses_lasso, dtype=float)
+    for i, degree in enumerate(self.degrees):
+      for j, hyperparameter in enumerate(self.hyperparameters):
+        model, mse, r2 = self.lasso(degree, hyperparameter)
+        self.mses_lasso[i, j] = mse
+        self.r2s_lasso[i, j] = r2
+    return self.mses_lasso, self.r2s_lasso
+
+  def visualize_mse_ols(self):
+    if not hasattr(self, 'mses_ols'):
+      self.mse_and_r2_ols()
+    fig, ax = plt.subplots(figsize=my_figsize())
+    ax.plot(self.degrees, self.mses_ols, label="MSE")
+    ax.set_xlabel("Polynomial degree")
+    ax.set_ylabel("MSE")
+    fig.tight_layout()
+    fig.savefig("mse_ols.pdf")
 
   def visualize_mse_ridge(self):
-      if not hasattr(self, 'mses_ridge'):
-        self.mse_and_r2_ridge()
-      fig, ax = plt.subplots(figsize=my_figsize())
-      degrees_mesh, hyperparameters_mesh = np.meshgrid(
-          self.degrees, self.hyperparameters)
-      levels = np.linspace(self.mses_ridge.min(), self.mses_ridge.max(), 7)
-      contour = ax.contourf(
-          degrees_mesh, hyperparameters_mesh, self.mses_ridge.T, levels=levels)
-      ax.set_yscale("log")
-      ax.set_xlabel("Complexity")
-      ax.set_ylabel("Ridge parameter")
-      ax.grid()
-      def format_func(x, _): return f"{x:.2f}"
-      cbar = plt.colorbar(contour, format=format_func)
-      fig.tight_layout()
-      fig.savefig("ridge_mse.pdf")
+    if not hasattr(self, 'mses_ridge'):
+      self.mse_and_r2_ridge()
+    fig, ax = plt.subplots(figsize=my_figsize())
+    degrees_mesh, hyperparameters_mesh = np.meshgrid(
+        self.degrees, self.hyperparameters)
+    levels = np.linspace(self.mses_ridge.min(), self.mses_ridge.max(), 7)
+    contour = ax.contourf(
+        degrees_mesh, hyperparameters_mesh, self.mses_ridge.T, levels=levels)
+    ax.set_yscale("log")
+    ax.set_xlabel("Polynomial degree")
+    ax.set_ylabel(r"hyperparameter $\lambda$")
+    ax.grid()
+    def format_func(x, _): return f"{x:.2f}"
+    cbar = plt.colorbar(contour, format=format_func)
+    cbar.set_label('MSE')
+    fig.tight_layout()
+    fig.savefig("ridge_mse.pdf")
+
+  def visualize_mse_lasso(self):
+    if not hasattr(self, 'mses_lasso'):
+      self.mse_and_r2_lasso()
+    fig, ax = plt.subplots(figsize=my_figsize())
+    degrees_mesh, hyperparameters_mesh = np.meshgrid(
+        self.degrees, self.hyperparameters)
+    levels = np.linspace(self.mses_lasso.min(), self.mses_lasso.max(), 7)
+    contour = ax.contourf(
+        degrees_mesh, hyperparameters_mesh, self.mses_lasso.T, levels=levels)
+    ax.set_yscale("log")
+    ax.set_xlabel("Polynomial degree")
+    ax.set_ylabel(r"hyperparameter $\lambda$")
+    ax.grid()
+    def format_func(x, _): return f"{x:.2f}"
+    cbar = plt.colorbar(contour, format=format_func)
+    cbar.set_label('MSE')
+    fig.tight_layout()
+    fig.savefig("lasso_mse.pdf")
