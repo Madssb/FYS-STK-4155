37428ac9739266dfea6dac4f1552210328968fdb
mads <anderen72@gmail.com>
2023-11-18 20:09:25 +0100

    ree

--- project_2/sklearn_nn.py
+++ project_2/sklearn_nn.py
@@ -6,23 +6,28 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 from sklearn.metrics import mean_squared_error
 
+
+def regression_1d_poly_2_deg():
 # Set a random seed for reproducibility
-np.random.seed(2023)
+  np.random.seed(2024)
+  rng = np.random.default_rng(2023)
+  n = 100
+  x = np.linspace(-10, 10, n)
+  y = 4 + 3 * x + x**2 + rng.normal(0, 0.1, n)
 
-n = 100
-x = np.linspace(-10, 10, n)
-y = 4 + 3 * x + x**2 + np.random.normal(0, 0.1, n)
+  X = np.array([np.ones(100), x, x**2]).T
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)  # Split the data into train and test sets
 
-X = np.array([np.ones(100), x, x**2]).T
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)  # Split the data into train and test sets
+  # Create and train the MLPRegressor
+  model = MLPRegressor(hidden_layer_sizes=(50, 50), max_iter=1000)
+  model.fit(X_train, y_train)
 
-# Create and train the MLPRegressor
-model = MLPRegressor(hidden_layer_sizes=(50, 50), max_iter=1000)
-model.fit(X_train, y_train)
+  # Make predictions on the test set
+  predicted = model.predict(X_test)
 
-# Make predictions on the test set
-predicted = model.predict(X_test)
+  # Calculate mean squared error
+  mse = mean_squared_error(y_test, predicted)
+  print("Mean Squared Error:", mse)
 
-# Calculate mean squared error
-mse = mean_squared_error(y_test, predicted)
-print("Mean Squared Error:", mse)
+
+regression_1d_poly_2_deg()
