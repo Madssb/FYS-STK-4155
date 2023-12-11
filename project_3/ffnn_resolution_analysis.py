58a057fee0fcb243c70e8b3fb80ab02bca89bfef
mads <anderen72@gmail.com>
2023-12-11 21:57:41 +0100

    made ffnn work again.

--- project_3/ffnn_resolution_analysis.py
+++ project_3/ffnn_resolution_analysis.py
@@ -1,6 +1,10 @@
 from PIL import Image
 import numpy as np
 from pathlib import Path
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+import matplotlib.pyplot as plt
 
 
 def read_image(
@@ -109,7 +113,72 @@ def features_and_labels_feed_forward_neural_network(image_size=(128, 128)):
     if not labels_filename.is_file():
         np.save(labels_filename, labels)
 
-if __name__ == "__main__":
+
+def initialize_features():
+    """Initialize features
+    """
     resolutions = [16, 32, 64, 128]
     for resolution in resolutions:
         features_and_labels_feed_forward_neural_network(image_size=(resolution, resolution))
+
+
+def temporary_name(feature_filename: str, architechture: tuple[int], plot=None):
+    """Train feed forward neural network.
+    
+    Parameters
+    ----------
+    feature_filename : str or Path
+        Filename for features stored as .npy.
+    architechture: Tuple[int].
+        Architechture of ffnn to be trained
+    """
+    labels = np.load("labels.npy")
+    features = np.load(feature_filename)
+    assert all(label for label in labels)
+    cloud_types = ["Ac", "As", "Cb", "Cc", "Ci", "Cs", "Ct", "Cu", "Ns", "Sc", "St"]
+    for label in labels:
+        assert label in cloud_types, f"{label=} not in {cloud_types=}"
+    label_encoder = LabelEncoder()
+    integer_labels =  label_encoder.fit_transform(np.array(labels).reshape(-1, 1))
+    features_train, features_test, labels_train, labels_test = train_test_split(features, integer_labels, random_state=1, shuffle=True)
+    clf = MLPClassifier(random_state=1, hidden_layer_sizes=architechture)
+    if plot:
+        
+        train_accuracies = []
+        test_accuracies = []
+        num_epochs = 10
+        for epoch in range(num_epochs):
+            try:
+                clf.partial_fit(features_train, labels_train)
+            except ValueError:
+                clf.partial_fit(features_train, labels_train, classes=np.unique(labels_train))
+
+
+            train_accuracy = clf.score(features_train, labels_train)
+            train_accuracies.append(train_accuracy)
+
+
+            test_accuracy = clf.score(features_test, labels_test)
+            test_accuracies.append(test_accuracy)
+
+            print(f"Epoch {epoch + 1}/{num_epochs}: Train Accuracy = {train_accuracy:.4f}, Test Accuracy = {test_accuracy:.4f}")
+        plt.plot(range(1, num_epochs + 1), train_accuracies, label="Train Accuracy")
+        plt.plot(range(1, num_epochs + 1), test_accuracies, label="Test Accuracy")
+        plt.xlabel("Epoch")
+        plt.ylabel("Accuracy")
+        plt.legend()
+        plt.show()
+    else:
+        clf.fit(features_train, labels_train)
+        train_accuracy = clf.score(features_train, labels_train)
+        test_accuracy = clf.score(features_test, labels_test)
+        print(f"{test_accuracy=:.4g}, {train_accuracy=:.4g}")
+    return clf, train_accuracies, test_accuracies
+
+
+
+if __name__ == "__main__":
+    temporary_name("features_ffnn_128x128.npy", (50,))
+    quit()
+    initialize_features()
+
